138693f9f193cb4789d3d6e18fd26d3abf18e78f
Melvin-4A6B <444277@edu.rocmn.nl>
2017-02-16 09:10:27 +0100

    nieuw invoerveld en knop toegevoegd

--- beoordelingenlijst.js
+++ beoordelingenlijst.js
@@ -13,3 +13,19 @@ function result() {
 		document.getElementById('resultaat').innerHTML = cijfer + ' bestaat niet';
 	}
 }
+
+function resultaat() {
+	var cijfer = document.getElementById('cijfers').value;
+
+	if(cijfer >= 0 && cijfer <= 49) {
+		document.getElementById('resultaat').innerHTML = 'De beoordeling is onvoldoende, want het cijfer is ' + cijfer + '.';
+	} else if(cijfer >= 50 && cijfer <= 59) {
+		document.getElementById('resultaat').innerHTML = 'De beoordeling is matig, want het cijfer is ' + cijfer + '.';
+	} else if(cijfer >= 60 && cijfer <= 74) {
+		document.getElementById('resultaat').innerHTML = 'De beoordeling is voldoende, want het cijfer is ' + cijfer + '.';
+	} else if(cijfer >= 75 && cijfer <= 100) {
+		document.getElementById('resultaat').innerHTML = 'De beoordeling is goed, want het cijfer is ' + cijfer + '.';
+	} else {
+		document.getElementById('resultaat').innerHTML = cijfer + ' bestaat niet';
+	}
+}
